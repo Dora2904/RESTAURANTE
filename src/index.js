4377b8fb90971404aa2322fff7e834097a827895
Carmen <carmentjfreelance@gmail.com>
2025-04-23 20:30:29 -0500

    Actualizacion con algunas modificaciones

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/database'); // Adjust the path as necessary
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');   
+app.use(express.json()); // Middleware to parse JSON requests
+
+connectDB(); // Connect to the database
+
+app.get("/", (req, res) => {
+    res.send('Bienivenido a la API del restaurante');   
 });
 const port = 3000;  
 app.listen(port, () => {
